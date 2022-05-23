09c83aeea4b70fe536b6a2c60c11e193fbc30268
abusenet <88513508+abusenet@users.noreply.github.com>
2022-05-23 17:49:41 -0500

    feat(serve): select file(s) to extract

--- nzb-serve.ts
+++ nzb-serve.ts
@@ -6,7 +6,8 @@ import {
   serve as serveHttp,
 } from "./deps.ts";
 
-import { NZB } from "./nzb.ts";
+import { File, NZB } from "./nzb.ts";
+import { extract } from "./nzb-extract.ts";
 
 const parseOptions = {
   string: [
@@ -31,23 +32,9 @@ export function help() {
   return `Usage: nzb-serve [...flags] <input>`;
 }
 
-const encoder = new TextEncoder();
-
-function serveNZB(_request: Request, nzb: NZB): Response {
-  const headers = new Headers();
-  headers.set("server", "deno");
-
-  // Set "accept-ranges" so that the client knows it can make range requests on future requests
-  headers.set("accept-ranges", "bytes");
-  headers.set("date", new Date().toUTCString());
-
-  headers.set("content-type", "text/html");
-
-  const page = encoder.encode(dirViewerTemplate(nzb));
-
-  return new Response(page, { status: 200, headers });
-}
-
+/**
+ * Serves an input NZB as a folder listing
+ */
 export async function serve(args = Deno.args) {
   const {
     _: [filename],
@@ -60,31 +47,90 @@ export async function serve(args = Deno.args) {
     return;
   }
 
+  const name: string = basename(filename as string, ".nzb");
+
   const nzb = await NZB.from(
     await Deno.open(filename as string),
-    basename(filename as string, ".nzb"),
+    name,
   );
 
-  await serveHttp((request: Request) => {
+  await serveHttp(async (request: Request): Promise<Response> => {
+    const { searchParams } = new URL(request.url);
+    const action = searchParams.get("action");
+
+    if (action === "extract") {
+      const formData = await request.formData();
+      const files = formData.getAll("files") as string[];
+
+      const headers = new Headers({
+        "Content-Type": "application/x-nzb",
+        "Content-Disposition": `attachment; filename="partial-${name}.nzb"`,
+      });
+
+      const stream = new TransformStream<Uint8Array, Uint8Array>();
+      extract([
+        filename as string,
+        files.map(escapeRegExp).join("|"),
+      ], {
+        out: stream.writable.getWriter(),
+      });
+
+      return new Response(stream.readable, { status: 200, headers });
+    }
+
     return serveNZB(request, nzb);
   }, options);
 }
 
-function dirViewerTemplate({ name, files }: NZB): string {
+function serveNZB(_request: Request, nzb: NZB): Response {
+  const headers = new Headers();
+  headers.set("server", "deno");
+
+  // Set "accept-ranges" so that the client knows it can make range requests on future requests
+  headers.set("accept-ranges", "bytes");
+  headers.set("date", new Date().toUTCString());
+
+  headers.set("content-type", "text/html");
+
+  const page = new TextEncoder().encode(
+    dirViewerTemplate(nzb.name!, nzb.files),
+  );
+
+  return new Response(page, { status: 200, headers });
+}
+
+function dirViewerTemplate(dirname: string, files: File[]): string {
+  const paths = dirname.split("/");
+
   return `
-    <h1>Index of ${name}</h1>
+    <h1>Index of
+      ${
+    paths.map((path, index, array) => {
+      if (path === "") return "";
+      const link = array.slice(0, index + 1).join("/");
+      return `<a href="${link}">${path}</a>`;
+    })
+      .join("/")
+  }
+    </h1>
+
     <table cellpadding="6">
       <thead>
         <tr>
+          <td></td>
           <th>Name</th>
           <th>Size</th>
           <th>Poster</th>
           <th>Last Modified Date</th>
         </tr>
       </thead>
+      <tbody>
       ${
     files.map((file) => `
         <tr>
+          <td>
+            <input type="checkbox" name="files" value="${file.name}" form="bulk" />
+          </td>
           <td>
             ${file.name}
           </td>
@@ -100,6 +146,20 @@ function dirViewerTemplate({ name, files }: NZB): string {
         </tr>
       `).join("")
   }
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>
+            <form id="bulk">
+              <button type="submit" formmethod="POST" formaction="/?action=extract">Extract</button>
+            </form>
+          </td>
+        </tr>
+      </tfoot>
     </table>
     `;
+}
+
+function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 }
