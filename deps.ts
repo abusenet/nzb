605606d4bd9a010752cfa9c65f6f72e360bf1b0c
abusenet <88513508+abusenet@users.noreply.github.com>
2022-06-11 15:54:55 -0500

    feat(mirror): more customization options
    - Progress report is optional, toggled by `--progress` flag.
    - Input can also be a remote URL
    - Can take a second argument with an object as defaults for flag.
      - `out` to override outpur writer.
      - `transform` to manual handle the mirroring of individual article.

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export { parse as parseFlags } from "https://deno.land/std@0.142.0/flags/mod.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.142.0/streams/conversion.ts";
 export { pooledMap } from "https://deno.land/std@0.142.0/async/pool.ts";
 export { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 export { basename, extname } from "https://deno.land/std@0.142.0/path/mod.ts";
@@ -12,4 +13,4 @@ export { ElementInfo, SAXParser } from "https://deno.land/x/xmlp@v0.3.0/mod.ts";
 export { Article, Client } from "https://deno.land/x/nntp@v0.4.0/mod.ts";
 export { retryAsync } from "https://deno.land/x/retry@v2.0.0/mod.ts";
 
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.7/mod.ts";
+export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
